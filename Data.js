88816887b41043ad98c1e438598dd6c6d6359859
unknown <harale507@gmail.com>
2024-02-03 16:05:08 +0200

    complete

---
+++ Data.js
@@ -0,0 +1,76 @@
+export const DATA = [
+    {
+        id : '1',
+        lastname : 'juntunen',
+        firstname: 'jaakko'
+    },
+    {
+        id : '2',
+        lastname : 'ojala',
+        firstname: 'tervo'
+    },
+    {
+        id : '3',
+        lastname : 'Kiri',
+        firstname: 'Maija'
+    },
+    {
+        id : '4',
+        lastname : 'Kankaanperä',
+        firstname : 'Katariina'
+    },
+    {
+        id : '5',
+        lastname : 'Jehkinen',
+        firstname : 'Henna'
+    },
+    {
+        id : '6',
+        lastname : 'Isometsä',
+        firstname : 'Lauri'
+    },
+    {
+        id : '7',
+        lastname : 'Uusitalo',
+        firstname : 'Miikka'
+    },
+    {
+        id : '8',
+        lastname : 'juntunen'
+    },
+    {
+        id : '9',
+        lastname : 'Halvari',
+        firstname : 'Veera'
+    },
+    {
+        id : '10',
+        lastname : 'Jurkiäinen',
+        firstname : 'Raine'
+    },
+    {
+        id : '11',
+        lastname : 'Tiisanoja',
+        firstname : 'Mika'
+    },
+    {
+        id : '12',
+        lastname : 'Merikanto',
+        firstname : 'Eeva'
+    },
+    {
+        id : '13',
+        lastname : 'Korkeaoja',
+        firstname : 'Sanna'
+    },
+    {
+        id : '14',
+        lastname : 'Katja',
+        firstname : 'Numminen'
+    },
+    {
+        id : '15',
+        lastname : 'Ristilä',
+        firstname : 'Mauno'
+    }
+]
